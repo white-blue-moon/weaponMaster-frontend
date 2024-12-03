79459da97d7d91e02bdba092fcbd7fdedb564253
white-blue-moon <eoiuco@gmail.com>
2024-12-03 16:10:58 +0900

    클라이언트 사이드 라우팅 적용

---
+++ src/routes/Home.svelte
@@ -0,0 +1,38 @@
+<script>
+    import GnbPublisher from '../components/GnbPublisher.svelte';
+    import Gnb from '../components/Gnb.svelte';
+    import Footer from '../components/Footer.svelte';
+</script>
+
+
+<GnbPublisher />
+<Gnb /> 
+
+<main>
+  <h1>Welcome to Weapon Master!</h1>
+  <p>서비스 소개...</p>
+</main>
+
+<Footer />
+
+<style>
+    main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
