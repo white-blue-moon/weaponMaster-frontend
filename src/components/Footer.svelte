6d0c7bc31e7f4595eab8ce03378f82156f9f5f8a
white-blue-moon <eoiuco@gmail.com>
2025-04-14 16:03:49 +0900

    neople Open API 로고 추가

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,10 +3,11 @@
   </script>
   
   <footer>
-    <p>이 홈페이지는 던전앤파이터 공식 홈페이지 리소스를 참고하여 제작되었으며, 비상업적 포트폴리오 제출용으로 사용되었습니다. <br> E-mail : soioiz@naver.com <br> &copy; 2005 NEOPLE Inc. & NEXON Korea Corporation All Rights Reserved.</p>
+    <p>이 홈페이지는 던전앤파이터 공식 홈페이지를 참고하여 제작되었으며, 비상업적 포트폴리오 제출용으로 사용되었습니다.<br> &copy; 2005 NEOPLE Inc. & NEXON Korea Corporation All Rights Reserved.</p>
     <li class="flogo">
       <a href="https://www.nexon.com/Home/Game" target="_blank"><img src="https://resource.df.nexon.com/ui/img/common/img_logo_nx.png" alt="넥슨"></a>
       <a href="https://www.neople.co.kr/" target="_blank"><img src="https://resource.df.nexon.com/ui/img/common/img_logo_np.png" alt="네오플"></a>
+      <a href="http://developers.neople.co.kr" target="_blank"><img src="/images/neople_openAPI_logo.png" alt="Neople 오픈 API" /></a>
     </li>
   </footer>
   
