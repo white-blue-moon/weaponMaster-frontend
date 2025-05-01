74d8be102591ec1a5397e485a6f31432e097e487
white-blue-moon <eoiuco@gmail.com>
2025-05-01 10:33:05 +0900

    홈페이지 자체 접근 제한 > 쿠키 방식 기본 틀 세팅

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,11 +1,17 @@
 import { writable } from "svelte/store";
+import { PATHS } from "../constants/paths";
 
-// 사용자 정보 저장소
+// 홈페이지 자체 접근 가능 여부
+export const canAccessPage = writable(
+    getCookieValue("canAccessPage") === "true"
+);
+
+// 사용자 정보 (아이디)
 export const userInfo = writable(
     JSON.parse(localStorage.getItem("userInfo")) || null
 );
 
-// 로그인 상태 저장소
+// 로그인 상태
 export const isLoggedIn = writable(
     JSON.parse(localStorage.getItem("isLoggedIn")) || false
 );
@@ -34,11 +40,28 @@ export function handleLogout() {
     isAdmin.set(false);
 
     alert("로그아웃 되었습니다.");
-    window.location.href = "/";
+    window.location.href = PATHS.HOME;
+    return;
 }
 
 // Caps Lock 감지 함수
 export function handleCapsLock(event, setCapsLockWarning) {
     const capsLockOn = event.getModifierState("CapsLock");
     setCapsLockWarning(capsLockOn);
+    return;
+}
+
+export function setCookie(name, value, day = 1) {
+    const expires  = new Date();
+    const oneDayMs = (24 * 60 * 60 * 1000);
+
+    expires.setTime(expires.getTime() + (day * oneDayMs)); // day 일 후 만료
+    document.cookie = `${name}=${encodeURIComponent(value)}; path=/; expires=${expires.toUTCString()};`;
+    return;
+}
+
+function getCookieValue(name) {
+    // 정규식: "쿠키이름=값;" > "쿠키이름, =, 값, ;" > 값
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? decodeURIComponent(match[2]) : null;
 }
