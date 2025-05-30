55389cce67f05a8fd21db9187e55e3d16cb5e786
white-blue-moon <eoiuco@gmail.com>
2025-05-30 11:30:43 +0900

    게시물 - 읽기 - 분류 출력 오류 수정

--- src/components/page/PageRead.svelte
+++ src/components/page/PageRead.svelte
@@ -133,7 +133,7 @@
         <h3>{ ARTICLE_TYPE_TEXT[article.categoryType][article.articleType] }</h3>
         <div class="board_view news_view">
             <dl>
-                <dt>{ getArticleFilterText(article.categoryType, article.articleType, article.articleDetailType) }</dt>
+                <dt>{ getArticleFilterText(article) }</dt>
                 <dd>
                     <p>
                         { article.title }
